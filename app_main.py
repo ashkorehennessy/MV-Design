9f573d365e500b6c0d1820f9f5eb54a5bdfd2a75
ashkorehennessy <ashkorehennessy@gmail.com>
2024-12-13 02:04:17 +0800

    UI调整

--- app_main.py
+++ app_main.py
@@ -6,7 +6,7 @@ import time
 import glob
 import numpy as np
 from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, \
-    QMessageBox, QComboBox, QCheckBox
+    QMessageBox, QComboBox, QCheckBox, QTextEdit
 from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot, Qt
 from PyQt5.QtGui import QImage, QPixmap
 from ultralytics import YOLO
@@ -35,10 +35,28 @@ MODEL_LIST = [
 
 TRACK_HISTORY = defaultdict(lambda: [])
 
+class OutputRedirector:
+    def __init__(self, text_edit, max_lines=1000):
+        self.text_edit = text_edit
+        self.max_lines = max_lines
+        self.text_edit.setDocument(self.text_edit.document())
+        self.text_edit.document().setMaximumBlockCount(self.max_lines)
+
+
+    def write(self, text):
+        # 将文本写入 QTextEdit 控件
+        self.text_edit.append(text)
+        self.text_edit.verticalScrollBar().setValue(self.text_edit.verticalScrollBar().maximum())
+
+    def flush(self):
+        # 必须实现 flush 方法
+        pass
+
 class VideoCaptureThread(QThread):
     frame_ready = pyqtSignal(np.ndarray)
     fps_updated = pyqtSignal(float)
     inference_time_updated = pyqtSignal(float)
+    detect_count_updated = pyqtSignal(int)
 
     def __init__(self):
         super().__init__()
@@ -49,6 +67,7 @@ class VideoCaptureThread(QThread):
         self.inference_time = 0.0
         self.current_model = None
         self.current_model_name = "不加载模型"
+        self.detect_count = 0
         self.enable_tracking = False
 
     def run(self):
@@ -58,7 +77,7 @@ class VideoCaptureThread(QThread):
             if not ret:
                 frame = np.zeros((height, width, 3), np.uint8)
                 self.video.release()
-                print("Camera disconnected. Attempting to reconnect...")
+                print("摄像头已掉线，正在尝试重新连接")
                 self.connect_camera()
 
             if ret:
@@ -73,20 +92,21 @@ class VideoCaptureThread(QThread):
                 self.fps_tick = current_time
                 self.fps_updated.emit(self.fps)
                 self.inference_time_updated.emit(self.inference_time)
+                self.detect_count_updated.emit(self.detect_count)
 
     def connect_camera(self):
         video_devices = glob.glob('/dev/video*')
         for device in video_devices:
-            print(f"Testing {device}")
+            print(f"正在检测 {device}")
             self.video = cv2.VideoCapture(device)
             if self.video.isOpened():
                 self.video.set(3, width)
                 self.video.set(4, height)
                 ret, _ = self.video.read()
                 if ret:
-                    print(f"Connected to {device}")
+                    print(f"成功连接到 {device}")
                     return
-        print("No available camera devices.")
+        print("没有可用的摄像头设备")
 
     def run_inference(self, frame):
         if self.current_model_name == "YOLO11_PT":
@@ -108,14 +128,24 @@ class VideoCaptureThread(QThread):
     def run_yolo11_pt_inference(self, frame):
         results = self.current_model(frame)
         frame = results[0].plot()
+        boxes = results[0].boxes.xywh.cpu().tolist()
+        confs = results[0].boxes.conf.cpu().tolist()
+        idx = 0
+        for box in boxes:
+            x, y, w, h = box
+            print(f"检测到人脸: (X:{x:.2f}, Y:{y:.2f}, 宽:{w:.2f}, 高:{h:.2f}), 可信度: {confs[idx]:.2f}")
+            idx += 1
+        self.detect_count = idx
         return frame
 
     def run_yolo11_pt_inference_track(self, frame):
         results = self.current_model.track(frame, persist=True)
         frame = results[0].plot()
+        idx = 0
         if results[0].boxes.id is not None:
             boxes = results[0].boxes.xywh.cpu()
             track_ids = results[0].boxes.id.int().cpu().tolist()
+            confs = results[0].boxes.conf.cpu().tolist()
             for box, track_id in zip(boxes, track_ids):
                 x, y, w, h = box
                 track = TRACK_HISTORY[track_id]
@@ -125,19 +155,31 @@ class VideoCaptureThread(QThread):
                 # Draw the tracking lines
                 points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
                 cv2.polylines(frame, [points], isClosed=False, color=(0, 255, 0), thickness=2)
+                print(f"检测到人脸: (X:{x:.2f}, Y:{y:.2f}, 宽:{w:.2f}, 高:{h:.2f}), 可信度: {confs[idx]:.2f}")
+                idx += 1
+        self.detect_count = idx
         return frame
 
     def run_yolo11_ncnn_inference(self, frame):
         results = self.current_model(frame, imgsz=export_imgsz)
         frame = results[0].plot()
+        boxes = results[0].boxes.xywh.cpu().tolist()
+        confs = results[0].boxes.conf.cpu().tolist()
+        idx = 0
+        for box in boxes:
+            x, y, w, h = box
+            print(f"检测到人脸: (X:{x:.2f}, Y:{y:.2f}, 宽:{w:.2f}, 高:{h:.2f}), 可信度: {confs[idx]:.2f}")
+            idx += 1
         return frame
 
     def run_yolo11_ncnn_inference_track(self, frame):
         results = self.current_model.track(frame, imgsz=export_imgsz, persist=True)
         frame = results[0].plot()
+        idx = 0
         if results[0].boxes.id is not None:
             boxes = results[0].boxes.xywh.cpu()
             track_ids = results[0].boxes.id.int().cpu().tolist()
+            confs = results[0].boxes.conf.cpu().tolist()
             for box, track_id in zip(boxes, track_ids):
                 x, y, w, h = box
                 track = TRACK_HISTORY[track_id]
@@ -146,6 +188,8 @@ class VideoCaptureThread(QThread):
                     track.pop(0)
                 points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
                 cv2.polylines(frame, [points], isClosed=False, color=(0, 255, 0), thickness=2)
+                print(f"检测到人脸: (X:{x:.2f}, Y:{y:.2f}, 宽:{w:.2f}, 高:{h:.2f}), 可信度: {confs[idx]:.2f}")
+                idx += 1
         return frame
 
     def run_haarcascades_inference(self, frame):
@@ -153,17 +197,24 @@ class VideoCaptureThread(QThread):
         faces = self.current_model.detectMultiScale(gray, 1.3, 5)
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            print(f"检测到人脸: (X:{x:.2f}, Y:{y:.2f}, 宽:{w:.2f}, 高:{h:.2f})")
+        self.detect_count = len(faces)
         return frame
 
     def run_yolo_fastestv2_inference(self, frame):
         results = self.current_model.infer(frame)
-        print("Inference result:", results)
+        idx = 0
         for box in results or []:
             x1, y1, x2, y2 = box["x1"], box["y1"], box["x2"], box["y2"]
+            w = x2 - x1
+            h = y2 - y1
             label = f'{box["class"]}: {box["score"]:.2f}'
             cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
             cv2.rectangle(frame, (x1, y1 - 17), (x1 + 100, y1), (255, 0, 0), -1)
             cv2.putText(frame, label, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            print(f"检测到人脸: (X:{x1:.2f}, Y:{y1:.2f}, 宽:{w:.2f}, 高:{h:.2f}), 可信度: {box['score']:.2f}")
+            idx += 1
+        self.detect_count = idx
         return frame
 
     def stop(self):
@@ -229,6 +280,7 @@ class CameraApp(QWidget):
         self.capture_thread.frame_ready.connect(self.update_frame)
         self.capture_thread.fps_updated.connect(self.update_fps)
         self.capture_thread.inference_time_updated.connect(self.update_inference_time)
+        self.capture_thread.detect_count_updated.connect(self.detect_count_updated)
         self.capture_thread.start()
 
     def init_ui(self):
@@ -236,16 +288,21 @@ class CameraApp(QWidget):
         self.image_label.setAlignment(Qt.AlignCenter)
         self.image_label.setText("正在初始化摄像头")
 
-        # Align above the combobox
-        self.fps_label = QLabel("当前帧率:0FPS", self)
+        # 右侧区域的标签、复选框和按钮
+        self.fps_label = QLabel("当前帧率: 0 FPS", self)
         self.fps_label.setAlignment(Qt.AlignLeft)
-        self.inference_time_label = QLabel("推理用时:0ms", self)
+
+        self.inference_time_label = QLabel("推理用时: 0 ms", self)
         self.inference_time_label.setAlignment(Qt.AlignLeft)
+
+        self.detect_count_label = QLabel("检测到人脸: 0", self)
+        self.detect_count_label.setAlignment(Qt.AlignLeft)
+
         self.choose_model_label = QLabel("选择模型:", self)
         self.choose_model_label.setAlignment(Qt.AlignLeft)
+
         self.enable_tracking_checkbox = QCheckBox("开启跟踪", self)
         self.enable_tracking_checkbox.stateChanged.connect(self.on_tracking_checkbox_changed)
-
 
         self.quit_button = QPushButton("退出系统", self)
         self.quit_button.clicked.connect(self.close_app)
@@ -255,18 +312,40 @@ class CameraApp(QWidget):
             self.model_select.addItem(model_name)
         self.model_select.currentIndexChanged.connect(self.on_model_change)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.image_label)
-        layout.addWidget(self.fps_label)
-        layout.addWidget(self.inference_time_label)
-        layout.addWidget(self.enable_tracking_checkbox)
-        layout.addWidget(self.choose_model_label)
-        layout.addWidget(self.model_select)
-        layout.addWidget(self.quit_button)
+        # 创建 QTextEdit 控件
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setReadOnly(True)  # 设置为只读
 
-        self.setLayout(layout)
+        # 左侧区域 (包含 image_label)
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(self.image_label)
+
+        # 右侧区域 (包含 fps, inference_time, model select 等)
+        right_layout = QVBoxLayout()
+        right_layout.addWidget(self.fps_label)
+        right_layout.addWidget(self.inference_time_label)
+        right_layout.addWidget(self.detect_count_label)
+        right_layout.addWidget(self.choose_model_label)
+        right_layout.addWidget(self.model_select)
+        right_layout.addWidget(self.enable_tracking_checkbox)
+        right_layout.addWidget(self.quit_button)
+
+        # 主布局 (包含左区域和右区域)
+        main_layout = QHBoxLayout()
+        main_layout.addLayout(left_layout, 1)  # 左边部分
+        main_layout.addLayout(right_layout, 1)  # 右边部分
+
+        # 添加 QTextEdit 到布局底部
+        bottom_layout = QVBoxLayout()
+        bottom_layout.addLayout(main_layout)
+        bottom_layout.addWidget(self.text_edit)
+
+        self.setLayout(bottom_layout)
         self.setWindowTitle("人脸检测系统")
-        self.resize(330, 370)
+        self.resize(540, 380)  # 根据实际需要调整窗口大小
+
+        self.redirector = OutputRedirector(self.text_edit)
+        sys.stdout = self.redirector
 
     def on_tracking_checkbox_changed(self, state):
         if state == Qt.Checked:
@@ -286,6 +365,9 @@ class CameraApp(QWidget):
 
     def update_inference_time(self, inference_time):
         self.inference_time_label.setText(f"推理用时:{inference_time*1000:.2f}ms")
+
+    def detect_count_updated(self, detect_count):
+        self.detect_count_label.setText(f"检测到人脸: {detect_count}")
 
     def load_model(self):
         selected_model_name = self.model_select.currentText()
--- exporter.py
+++ exporter.py
@@ -2,7 +2,7 @@ from sympy import simplify
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("./best.pt")  # load an official model
+model = YOLO("./yolo11_640.pt")  # load an official model
 
 # Export the model
-model.export(format="ncnn",imgsz=160,int8=True,data="yolo11n_train/face.yaml")
+model.export(format="onnx",data="yolo11n_train/face.yaml")
